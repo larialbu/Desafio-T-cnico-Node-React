fab0dcedc0435c3cb12d136e8d7f71aacf50dbcb
larialbu <larissaamca@gmail.com>
2023-01-13 21:58:09 -0300

    updates

---
+++ frontend/src/components/Form.js
@@ -0,0 +1,125 @@
+import axios from "axios";
+import React, { useEffect, useRef } from "react";
+import styled from "styled-components";
+import { toast } from "react-toastify";
+
+const FormContainer = styled.form`
+  display: flex;
+  align-items: flex-end;
+  gap: 10px;
+  flex-wrap: wrap;
+  background-color: #fff;
+  padding: 20px;
+  box-shadow: 0px 0px 5px #ccc;
+  border-radius: 5px;
+`;
+
+const InputArea = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Input = styled.input`
+  width: 120px;
+  padding: 0 10px;
+  border: 1px solid #bbb;
+  border-radius: 5px;
+  height: 40px;
+`;
+
+const Label = styled.label``;
+
+const Button = styled.button`
+  padding: 10px;
+  cursor: pointer;
+  border-radius: 5px;
+  border: none;
+  background-color: #2c73d2;
+  color: white;
+  height: 42px;
+`;
+
+const Form = ({ getUsers, onEdit, setOnEdit }) => {
+  const ref = useRef();
+
+  useEffect(() => {
+    if (onEdit) {
+      const user = ref.current;
+
+      user.nome.value = onEdit.nome;
+      user.descricao.value = onEdit.email;
+      user.hora_inicio.value = onEdit.hora_inicio;
+      user.hora_termino.value = onEdit.hora_termino;
+    }
+  }, [onEdit]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const user = ref.current;
+
+    if (
+      !user.nome.value ||
+      !user.descricao.value ||
+      !user.hora_inicio.value ||
+      !user.hora_termino.value
+    ) {
+      return toast.warn("Preencha todos os campos!");
+    }
+
+    if (onEdit) {
+      await axios
+        .put("http://localhost:8800/" + onEdit.id, {
+          nome: user.nome.value,
+          descricao: user.descricao.value,
+          hora_inicio: user.hora_inicio.value,
+          hora_termino: user.hora_termino.value,
+        })
+        .then(({ data }) => toast.success(data))
+        .catch(({ data }) => toast.error(data));
+    } else {
+      await axios
+        .post("http://localhost:8800", {
+          nome: user.nome.value,
+          descricao: user.descricao.value,
+          hora_inicio: user.hora_inicio.value,
+          hora_termino: user.hora_termino.value,
+        })
+        .then(({ data }) => toast.success(data))
+        .catch(({ data }) => toast.error(data));
+    }
+
+    user.nome.value = "";
+    user.descricao.value = "";
+    user.hora_inicio.value = "";
+    user.hora_termino.value = "";
+
+    setOnEdit(null);
+    getUsers();
+  };
+
+  return (
+    <FormContainer ref={ref} onSubmit={handleSubmit}>
+      <InputArea>
+        <Label>Nome</Label>
+        <Input name="nome" />
+      </InputArea>
+      <InputArea>
+        <Label>Descrição</Label>
+        <Input name="descricao" type="text" />
+      </InputArea>
+      <InputArea>
+        <Label>Hora de inicio</Label>
+        <Input name="hora_inicio" type="time" />
+      </InputArea>
+      <InputArea>
+        <Label>Hora de termino</Label>
+        <Input name="hora_termino" type="time" />
+      </InputArea>
+
+      <Button type="submit">SALVAR</Button>
+    </FormContainer>
+  );
+};
+
+export default Form;
---
+++ frontend/src/components/login.js
@@ -0,0 +1,42 @@
+import{Link} from 'react-router-dom'
+import styles1 from './login.module.css'
+import { FaEnvelope, FaLock, FaFacebook, FaGoogle, FaGithub } from 'react-icons/fa' ;
+
+function Login(){
+    return(
+        <div className={styles1.body}>
+            <div className={styles1.container1}>
+                <div className={styles1.loginForm1}>
+                    <div className={styles1.title1}>Login</div>
+                    <form action='#'>
+                        <div className={styles1.inputBox1}>
+                        <FaEnvelope className={styles1.icons1}/>
+                        <input type="text" placeholder='Enter your email' required/>
+                        </div>
+
+                        <div className={styles1.inputBox1}>
+                            <FaLock className={styles1.icons1}/>
+                        <input type="password" placeholder='Enter your password' required/>
+                        </div> 
+                        <div className={styles1.fotgot1}> <Link to='./RecuperarSenha' className={styles1.a}>Esqueceu a senha?</Link></div>
+                        <div className={styles1.inputBox1}>
+                            <input type="submit" vaule="Entrar"/>
+                        </div>
+                        <div className={styles1.divider1}>ou</div>
+                        <div className={styles1.medias1}>
+                            <FaFacebook className={styles1.icons1}/>
+                            <FaGoogle className={styles1.icons1}/>
+                            <FaGithub className={styles1.icons1}/>
+                        </div>
+                        <div className={styles1.sigupText1}>
+                            Não tenho uma conta? <Link to='./CadastroUsuario'>Crie uma agora</Link>
+                        </div>
+                    </form>
+                </div>
+
+            </div>
+        </div>
+    );
+}
+
+export default Login; 
